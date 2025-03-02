8f50a9965ac9536e25e0d66d77df2e3914acd4fd
Dhruv Yadav <dky1999@gmail.com>
2025-03-02 20:35:47 +0530

    pokkj

--- src/main/java/com/security/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/security/serviceImpl/UserServiceImpl.java
@@ -27,4 +27,10 @@ public class UserServiceImpl implements UserService{
 		return repository.findAll();
 	}
 
+	@Override
+	public List<User> gatUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
